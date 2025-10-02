83b9440811a47673914eda0e9a44319acbb63789
Timo Sarkar <timosarkartimo@gmail.com>
2025-10-02 16:16:48 +0200

    Update capstone.c

--- capstone.c
+++ capstone.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #include <capstone/capstone.h>
 
-extern int main(); // anchor for program text
-
-int test_capstone_program() {
+int disasm_function(void *func_ptr, int size) {
     csh handle;
     cs_insn *insn;
     size_t count;
 
-    // Initialize Capstone for ARM64
-    if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK) {
+    if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
         return -1;
-    }
 
-    // Pointer to start of main; disassemble ~1KB of code
-    unsigned char *start = (unsigned char*)&main;
-    size_t size = 1024; // adjust if you want more
+    unsigned char *code = (unsigned char*)func_ptr;
 
-    count = cs_disasm(handle, start, size, (uint64_t)start, 0, &insn);
+    count = cs_disasm(handle, code, size, (uint64_t)code, 0, &insn);
 
     if (count > 0) {
-        for (size_t i = 0; i < count; i++) {
+        for (size_t i = 0; i < count; i++)
             printf("0x%llx:\t%s\t%s\n",
                 insn[i].address,
                 insn[i].mnemonic,
                 insn[i].op_str);
-        }
+
         cs_free(insn, count);
     }
 
