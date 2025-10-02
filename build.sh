3b731ebd84c98fc62ec7fefaf990da588ed57cc1
Timo Sarkar <timosarkartimo@gmail.com>
2025-10-02 16:04:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 llc -filetype=obj main.ll -o main.o
 clang -c capstone.c -I/opt/homebrew/include -o capstone.o
-clang -c myfunc.c -o myfunc.o 
-clang main.o capstone.o myfunc.o -L/opt/homebrew/lib -lcapstone -o test_capstone
+clang main.o capstone.o -L/opt/homebrew/lib -lcapstone -o test_capstone
